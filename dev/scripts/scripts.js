de7091453e6c8b775fc6e4c990076095a80f1d65
Cody Choi <cchoi_2294@hotmail.com>
2017-08-31 17:59:51 -0400

    matched cards no longer flip back

--- dev/scripts/scripts.js
+++ dev/scripts/scripts.js
@@ -2,6 +2,8 @@ var cardGame = {};
 cardGame.key = '6cc621452cadd6d6f867f4435723803f';
 cardGame.dogPics = [];
 cardGame.randPics = [];
+cardGame.gameStart = false;
+cardGame.previous = "";
 
 // User should press 'Start', fadeIn instructions on top with an "x" to close and a button close
 // Loading screen, if needed, while AJAX calls request pics of doges
@@ -42,7 +44,6 @@ cardGame.getContent = () => {
             cardGame.randPics.push(cardGame.dogPics[randomPick]);
             cardGame.randPics.push(cardGame.dogPics[randomPick]);
         }
-
         cardGame.displayContent();
     });
 }
@@ -51,8 +52,8 @@ cardGame.events = () => {
     $('.startBtn').on('click', () => {
         swal({
             title: "Sweet!",
-            text: "Here's a custom image.",
-            imageUrl: "images/thumbs-up.jpg"
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos architecto quaerat omnis minus excepturi ut praesentium, soluta laudantium perspiciatis inventore? Ea assumenda tempore natus ducimus ipsum laudantium officiis, enim voluptas.",
+            imageUrl: "https://i.pinimg.com/736x/f2/41/46/f24146096d2f87e31745a182ff395b10--pug-cartoon-art-ideas.jpg"
         }, () => {
             cardGame.getContent();
         });
@@ -60,14 +61,30 @@ cardGame.events = () => {
 }
 
 cardGame.matchGame = () => {
-    $('.card').on('click', (e) => {
+    let counter = 0;
+    cardGame.previous = '';
+    let current = '';
+
+    $('.card').on('click', function (e) {
         e.preventDefault();
         e.stopPropagation();
+        cardGame.gameStart = true;
+        counter++;
         let c = e.currentTarget.classList;
         if (c.contains('flipped') === true) {
             c.remove('flipped');
+            console.log("remove flip");
         } else {
             c.add('flipped');
+        }
+
+        if (counter === 2) {
+            cardGame.gameFx($(this), cardGame.previous);
+            counter = 0;
+        } else if (counter === 1) {
+            cardGame.previous = $(this);
+        } else {
+            counter = 0;
         }
     });
 }
@@ -83,30 +100,46 @@ cardGame.displayContent = () => {
         let picString = picsToUse.splice(randPic, 1);
         $(el).attr('style', `background-image: url(${picString[0]})`);
         $(el).addClass(className);
-        cardGame.gameFx(picString);
     });
+    cardGame.matchGame();
 }
 
-cardGame.gameFx = (pic) => {
-    $('.card').on('click', (e) => {
-        let c = e.currentTarget.classList;
-        if (c.contains('flipped') && pic = cardGame.randPics){
-            console.log('hi');
-        }
-});
-                  
+cardGame.gameFx = (current, prev) => {
+    let currentDogPicsClass = "";
+    currentDogPicsClass = current.children(".card__front").attr('class');
+    currentDogPicsClass = "." + currentDogPicsClass.replace("card__front ", "");
+    let previousDogPicsClass = "";
+    previousDogPicsClass = prev.children(".card__front").attr('class');
+    previousDogPicsClass = "." + previousDogPicsClass.replace("card__front ", "");
+    if ($(currentDogPicsClass).css('background-image') === $(previousDogPicsClass).css('background-image')) {
+        current.addClass('match');
+    } else {
+        setTimeout( () => {
+            current.removeClass('flipped');
+            prev.removeClass('flipped');            
+        },1500);
+    }
 
-    
+
+
+
+
+
+    //    if ($('').css('background-image') === $('').css('background-image')) {
+    //        
+    //    }
+}
+
+
+
+
+
 //    3. Compare the pictures (aka the value or id) and if equal, then match = true, else flip them back over. If match = true, cards stay flipped.
-    
-    //when two cards are flipped and pic === pic, than stay flipped
-};
 
 
 
 cardGame.init = () => {
     cardGame.events();
-    cardGame.matchGame();
 };
 
 $(() => {
